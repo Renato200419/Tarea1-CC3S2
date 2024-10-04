fe4d4742a32f019af54e647f4a6e4a3fb072f32b
Renato200419 <renato.olivera.c@uni.pe>
2024-10-04 00:11:37 -0500

    Actualizar archivo.py

--- tests/integration/test_questions_integration.py
+++ tests/integration/test_questions_integration.py
@@ -1,6 +1,6 @@
 import httpx
 from fastapi.testclient import TestClient
-from app.main import app  # Esta línea ajusta esta importación si `main.py` está en otra ubicación
+from ..app.main import app  # Esta línea ajusta esta importación si `main.py` está en otra ubicación
 
 # Crear un cliente de pruebas para la aplicación FastAPI
 client = TestClient(app)
